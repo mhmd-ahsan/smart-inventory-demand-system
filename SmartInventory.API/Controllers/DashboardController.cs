ef54a877c808323bbbe3f82a322af56fae25da99
Muhammad Ahsan <ahsantahirmuhammad2003@gmail.com>
2026-05-04 18:00:22 +0500

    Dashboard COntroller and Program cs

---
+++ SmartInventory.API/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartInventory.Application.Interfaces.Service_Interfaces.Dashboard_Interface;
+using SmartInventory.Application.Services;
+
+namespace SmartInventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _service;
+
+        public DashboardController(IDashboardService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("analytics")]
+        public async Task<IActionResult> GetDashboardAnalytics()
+        {
+            var response = await _service.GetDashboardDataAsync();
+
+            if (!response.Success)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+    }
+}
