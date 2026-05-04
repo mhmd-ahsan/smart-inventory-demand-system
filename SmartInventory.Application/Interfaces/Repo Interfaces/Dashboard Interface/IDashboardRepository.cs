17c1bf717d60b16febc86c5c1a2ec9e2820b5c54
Muhammad Ahsan <ahsantahirmuhammad2003@gmail.com>
2026-05-04 18:00:46 +0500

    Dashboard Component analystics

---
+++ SmartInventory.Application/Interfaces/Repo Interfaces/Dashboard Interface/IDashboardRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartInventory.Application.DTOs.DashboardDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Application.Interfaces.Repo_Interfaces.Dashboard_Interface
+{
+    public interface IDashboardRepository
+    {
+        Task<DashboardReadDto> GetDashboardDataAsync();
+    }
+}
---
+++ SmartInventory.Application/Interfaces/Service Interfaces/Dashboard Interface/IDashboardService.cs
@@ -0,0 +1,15 @@
+﻿using SmartInventory.Application.Common.Responses;
+using SmartInventory.Application.DTOs.DashboardDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Application.Interfaces.Service_Interfaces.Dashboard_Interface
+{
+    public interface IDashboardService
+    {
+        Task<ServiceResponse<DashboardReadDto>> GetDashboardDataAsync();
+    }
+}
